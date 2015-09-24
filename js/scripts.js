9b6bd1451e679553aab81cfb367ffca08e5874c5
nptrsn <nick@hindiemedia.com>
2015-09-23 22:47:25 -0700

    initial html/css before transition from columns to rows

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Empty JS for your own code to be here
+
+// Change Mailchimp placehoder
+$( window ).load(function() {
+  $( 'input#mce-EMAIL.email' ).attr("placeholder", "Sign up for our mailing list");
+  $( 'input#mc-embedded-subscribe.button').attr('value','Join');
+});
